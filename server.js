0a2591a666deefce3ff5798492d66919b9b3e0eb
Diallo224-timbi <maouloudoutimbi@gmail.com>
2024-12-06 12:07:34 +0100

    modifactionPort

--- server.js
+++ server.js
@@ -5,13 +5,14 @@ const path = require("path"); // importe le module path pour gerer les chemin
 const app = express(); // créer une instance pour configurer le serveur
 
 // se servir uniquement des fichier static du dossier dist
-app.use(express.static(path.join(__dirname +"/dist/assignment-app/browser")));
+app.use(express.static(__dirname +"/dist/assignment-app/browser"));
 
 app.get("/*", function (req,res) {
     res.sendFile(path.join(__dirname + "/dist/assignment-app/browser/index.html"));
+    console.log("connecter")
 })
 
 // demarer l'application en ecoutant sur le port render
-app.listen(process.env.PORT || 8081, () => {
-    console.log("Server is running on port", process.env.PORT || 8081);
+app.listen(process.env.PORT || 8010, () => {
+    console.log("Server demarer avec le port", process.env.PORT || 8010);
 });
